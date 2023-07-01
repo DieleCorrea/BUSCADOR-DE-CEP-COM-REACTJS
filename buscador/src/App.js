adc4a1bc1d3e5ba685a58ccf7abc972349bc8147
Diele <dybs1520@gmail.com>
2023-06-30 21:39:14 -0300

    Update App.js
    Coloquei um título “Buscador de cep” e o input que vai receber um texto do usuário

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -2,8 +2,18 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>TESTE PROJETO</h1>
+    <div className="container">
+      <h1 className="title">Buscador de cep</h1>
+
+      <div className="containerImput">
+      
+      <input
+      type="text"
+      placeholder="Digite seu cep " //esse placeholder é pra quando a pessoa não tiver feito nada ainda e aparecer isso
+      />
+
+      </div>
+
     </div>
   );
 }
