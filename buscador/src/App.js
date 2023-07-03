114f13984069e24cb4c872d85b20f66d947047bb
Diele <dybs1520@gmail.com>
2023-07-03 14:59:01 -0300

    ícone branco
    Para fazer o icone branco, instalei a biblioteca do react e importei no app.js  "react-icons"

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,18 +1,23 @@
 
-
+import { FiSearch} from 'react-icons/fi'
 function App() {
   return (
     <div className="container">
       <h1 className="title">Buscador de cep</h1>
 
-      <div className="containerImput">
+      <div className="containerImput">   
       
-      <input
-      type="text"
-      placeholder="Digite seu cep " //esse placeholder é pra quando a pessoa não tiver feito nada ainda e aparecer isso
-      />
-
+        <input
+          type="text"
+          placeholder="Digite seu cep " //esse placeholder é pra quando a pessoa não tiver feito nada ainda e aparecer isso
+          />
+        <button className="buttonSearch">
+        <FiSearch 
+          size={25}
+          color="#FFF" />
+        </button>
       </div>
+
 
     </div>
   );
