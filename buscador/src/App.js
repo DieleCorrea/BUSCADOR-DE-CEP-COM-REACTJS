2a73bcc2ce247bc487bb82c35560d70c450a1f6b
Diele <dybs1520@gmail.com>
2023-07-05 17:38:03 -0300

    Import useState
    Precisamos ter algo dinâmico, pois quando digitamos algo ali na busca, temos que salvar em uma variável e depois quando clicar enviar para a API que vamos usar para buscar o cep!
    
    Quando precisamos trabalhar com algo dinâmico, ou seja, que muda o valor conforme a ação, nós usamos um “estado” ou seja um useState que vem do próprio React.
    -Importamos no nosso App.js e criei dentro do nosso componente function App. No caso vai começar vazio  para colocar o que as pessoas vão digitar  e salvar dentro do meu input, para salvar, tem uma propriedade que vamos chamar de onChange.

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,8 +1,13 @@
 
+import { useState } from 'react'; 
 import { FiSearch} from 'react-icons/fi'
 import './styles.css';
 
 function App() {
+
+  const[ input, setImput] = useState('')
+  /* Input é o nome do estado e o setImput é o nome da função que temos  para trocar  valor o estado.
+  Quando eu chamar o Input apenas, eu to querendo apenas saber  nome do estado. Quandoo chamo o setImput é para eu PASSAR UM VALOR NOVO PARA ESSE ESTADO */
   return (
     <div className="container">
       <h1 className="title">Buscador de cep</h1>
@@ -12,6 +17,10 @@ function App() {
         <input
           type="text"
           placeholder="Digite seu cep " //esse placeholder é pra quando a pessoa não tiver feito nada ainda e aparecer isso
+          value={input}/*o valor do input estara sempre atrelado ao nosso usaeState input */
+          onChange={(e) => setImput(e.target.value)}
+          /*recebbemos uma função anonima que chamei de "e" e passamos para  set input onde recebemos o evento e temos acesso ao valor dgitado através do "e.target.value" */
+
           />
         <button className="buttonSearch">
         <FiSearch 
